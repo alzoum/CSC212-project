1ab6b38c59f865f8daf1a40d5f428a29b6100039
fhd-4 <fhd-4@DESKTOP-QQOG25I>
2023-10-04 20:34:34 +0300

    final final

--- Linked List Phonebook/src/LinkedList.java
+++ Linked List Phonebook/src/LinkedList.java
@@ -15,19 +15,6 @@ public class LinkedList<T> {
 		return head == null;
 	}
 
-//	public void insertContact(Contact contact) {
-//		Node<Contact> newNode = new Node<>(contact);
-//		if (head == null) {
-//			head = newNode;
-//		} else {
-//			Node<Contact> current = head;
-//			while (current.getNext() != null) {
-//				current = current.getNext();
-//			}
-//			current.setNext(newNode);
-//		}
-//	}
-	
 	public void insert(T e) {
 		if (e instanceof Event) {
 			Node<Event> newNode = new Node<>((Event) e);
@@ -62,26 +49,6 @@ public class LinkedList<T> {
 		}
 	}
 
-//	public boolean remove(T e) {
-//		if (isEmpty()) {
-//			return false;
-//		} else if (head.getData().equals(e)) {
-//			head = head.getNext();
-//			return true;
-//		} else {
-//			Node<Contact> current = head;
-//			
-//			while (current.getNext() != null) {
-//				if (current.getNext().getData().equals(e)) {
-//					current.setNext(current.getNext().getNext());
-//					return true;
-//				}
-//				current = current.getNext();
-//			}
-//		}
-//		return false;
-//	}
-	
 	public boolean remove(T e) {
 		if (isEmpty()) {
 			return false;
@@ -112,7 +79,6 @@ public class LinkedList<T> {
 		return false;
 	}
 
-	
 	public LinkedList<Contact> searchContactsByCriteria(String searchCriteria, String searchValue) {
 		LinkedList<Contact> searchResults = new LinkedList<>();
 
@@ -143,18 +109,6 @@ public class LinkedList<T> {
 		return searchResults;
 	}
 
-//	public Contact search(String value) {
-//		Node<Contact> current = getHead();
-//		while (current != null) {
-//			if (((Contact) current.getData()).getName().equalsIgnoreCase(value)
-//					|| ((Contact) current.getData()).getPhoneNumber().equalsIgnoreCase(value)) {
-//				return (Contact) current.getData();
-//			}
-//			current = current.getNext();
-//		}
-//		return null;
-//	}
-	
 	public T search(String value) {
 		Node<T> current = head;
 
@@ -166,7 +120,8 @@ public class LinkedList<T> {
 				}
 			} else if (current.getData() instanceof Event) {
 				Event event = (Event) current.getData();
-				if (value.equalsIgnoreCase(event.getTitle()) || value.equalsIgnoreCase(event.getEventUser().getName())) {
+				if (value.equalsIgnoreCase(event.getTitle())
+						|| value.equalsIgnoreCase(event.getEventUser().getName())) {
 					return current.getData();
 				}
 			}
@@ -194,29 +149,11 @@ public class LinkedList<T> {
 		}
 	}
 
-//	public void insertEvent(Event e) {
-//		Node<Event> newNode = new Node<>(e);
-//
-//		if (head == null || e.getTitle().compareToIgnoreCase(((Event) head.getData()).getTitle()) < 0) {
-//			newNode.setNext(head);
-//			head = newNode;
-//		} else {
-//			Node<Event> current = head;
-//			while (current.getNext() != null
-//					&& e.getTitle().compareToIgnoreCase(((Event) current.getNext().getData()).getTitle()) >= 0) {
-//				current = current.getNext();
-//			}
-//			newNode.setNext(current.getNext());
-//			current.setNext(newNode);
-//		}
-//	}
-	
 	public void scheduleEvent(String title, String date, String time, String location, Contact eventUser) {
 
 		if (eventUser != null) {
 			boolean hasConflict = false;
 			Event newEvent = new Event(title, date, time, location, eventUser);
-			
 
 			Node current = head;
 			while (current != null) {
@@ -246,7 +183,7 @@ public class LinkedList<T> {
 			System.out.println("\nContact does not exist in the phonebook.\n");
 		}
 	}
-	
+
 	public void printAllEvents() {
 		Node<Event> current = head;
 
